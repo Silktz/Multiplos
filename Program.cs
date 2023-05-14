f872d90d221bc0649817bf6afc9a2bc0c571cdb4
Silktz <caua.franco3@etec.sp.gov.br>
2023-05-14 19:49:00 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//By Silktz 
+Console.WriteLine("Hello, World!");
+
+int numero, multiplo, resultado;
+
+Console.WriteLine("Digite qualquer número: ");
+numero = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Digite um numero a ser verificado como múltiplo: ");
+multiplo = int.Parse(Console.ReadLine()!);
+
+if (numero % multiplo == 0)
+{
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($"{numero} é multiplo por {multiplo}");
+    }
+
+    else 
+    {
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{numero} não é multiplo por {resultado=multiplo}");
+    }
+
+
+
